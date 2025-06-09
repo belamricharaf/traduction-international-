213e9b7fe655817095c458ec76fc06d2e975cfd2
belamricharaf <charafpicso22@gmail.com>
2025-06-09 09:01:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Header, HTTPException
+from models import TranslationRequest
+from translation import translate_text
+
+app = FastAPI()
+
+API_KEY = "123456789-SECRET"
+
+@app.post("/translate")
+def translate(req: TranslationRequest, authorization: str = Header(...)):
+    if authorization != f"Bearer {API_KEY}":
+        raise HTTPException(status_code=403, detail="Clé API invalide")
+
+    translated = translate_text(req.text, req.source_language, req.target_language)
+    return {"translated_text": translated}
